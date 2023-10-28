ccd6e06902ef4d036b283937f7bb97e512559865
RaySlash <stevemathewjoy@gmail.com>
2023-10-28 08:46:44 +0000

    frost: add timezone and console fonttype

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -84,6 +84,12 @@
     driSupport32Bit = true;
   };
 
+  time.timeZone = "Australia/Brisbane";
+	i18n.defaultLocale = "en_US.UTF-8";
+	console = {
+		font = "Lat2-Terminus16";
+		keyMap = "us";
+	};
   fonts.fonts = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
