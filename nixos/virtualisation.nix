8ec8b0e40517f348bef3405a8b4ce8f08cf635f6
RaySlash <stevemathewjoy@gmail.com>
2023-11-08 19:08:29 +1000

    frost: update virt and misc

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -1,6 +1,9 @@
 { config, pkgs, inputs, outputs, ... }: {
 
-virtualisation.libvirtd.enable = true;
+virtualisation.libvirtd = {
+  enable = true;
+  qemu.swtpm.enable = true;
+};
 programs.dconf.enable = true; 
 environment.systemPackages = with pkgs; [ virt-manager ];
 
