fbc73f69706974be3d9d4b004e56c0529b875ac6
RaySlash <stevemathewjoy@gmail.com>
2023-11-16 19:33:53 +1000

    dell: add dell and config

---
+++ home-manager/dell/services.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, inputs, outputs, ... }:  {
+
+  programs = {
+    gpg.enable = true;
+    git = {
+			enable = true;
+			userEmail = "stevemathewjoy@gmail.com";
+			userName = "RaySlash";
+		};
+    alacritty = {
+			enable = true;
+      package = pkgs.unstable.alacritty;
+      settings = {
+        window = {
+          colors = {
+            primary = {
+              background =  "#282828";
+              foreground =  "#ebdbb2";
+            };
+            normal = { 
+              black =    "#282828";
+              red =      "#cc241d";
+              green =    "#98971a";
+              yellow =   "#d79921";
+              blue =     "#458588";
+              magenta =  "#b16286";
+              cyan =     "#689d6a";
+              white =    "#a89984";
+            };
+
+            bright = { 
+              black =    "#928374";
+              red =      "#fb4934";
+              green =    "#b8bb26";
+              yellow =   "#fabd2f";
+              blue =     "#83a598";
+              magenta =  "#d3869b";
+              cyan =     "#8ec07c";
+              white =    "#ebdbb2";
+            };
+          };
+          decorations = "full";
+					opacity = 0.9;
+					dynamic_title = true;
+					history = 100000;
+					multiplier = 2;
+					cursor ={
+						text = "CellBackground";
+						cursor = "CellForeground";
+					};
+					vi_mode_cursor = {
+						text = "CellBackground";
+						cursor = "CellForeground";
+					};
+					transparent_background_colors = false;
+				};
+				bell = {
+					animation = "EaseOutExpo";
+					duration = 0;
+					color = "#ffffff";
+				};
+				cursor = {
+					style = {
+						shape = "Block";
+						blinking = "On";
+					};
+					vi_mode_style = "None";
+					blink_interval = 750;
+					blink_timeout = 0;
+					live_config_reload = true;
+				};
+			}; 
+		};
+  };
+}
---
+++ nixos/dell/services.nix
@@ -0,0 +1,36 @@
+{ inputs, outputs, config, pkgs, ... }: {
+
+  services = {
+    openssh = {
+      enable = true;
+    };
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+  };
+
+  programs = {
+    dconf.enable = true;
+    zsh = {
+      enable = true;
+      syntaxHighlighting.enable = true;
+      autosuggestions.enable = true;
+      zsh-autoenv.enable = true;
+      enableCompletion = true;
+      histSize = 10000;
+      ohMyZsh = {
+        enable = true;
+        theme = "robbyrussell";
+      };
+      shellAliases = {
+        ls = "exa --icons";
+        ll = "exa --icons -l";
+        vim = "nvim";
+        nix-update = "sudo nixos-rebuild switch";
+      };
+    };
+  };
+                                        }
