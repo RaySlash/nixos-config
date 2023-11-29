ea61505b5d825740f84b011587e54d5912411055
RaySlash <stevemathewjoy@gmail.com>
2023-11-29 14:27:02 +1000

    frost: add automatic nix gc (weekly)

--- nixos/frost/configuration.nix
+++ nixos/frost/configuration.nix
@@ -38,6 +38,11 @@
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
     };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
   };
 
   home-manager = {
