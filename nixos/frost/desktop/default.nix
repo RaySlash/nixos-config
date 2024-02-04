68499bd96bc9483cc2ee1120f37a825d1d7c0918
RaySlash <stevemathewjoy@gmail.com>
2024-02-04 23:02:03 +1000

    frost: update xkbvariant and xkblayout

--- nixos/frost/desktop/default.nix
+++ nixos/frost/desktop/default.nix
@@ -1,37 +1,37 @@
-{ config, pkgs, lib, inputs, outputs, ... }:
-
-let 
-  sddm-chili-theme = pkgs.libsForQt5.callPackage (inputs.nixpkgs + "/pkgs/data/themes/chili-sddm/default.nix") {};
+{ config
+, pkgs
+, lib
+, inputs
+, outputs
+, ...
+}:
+let
+  sddm-chili-theme =
+    pkgs.libsForQt5.callPackage
+      (inputs.nixpkgs + "/pkgs/data/themes/chili-sddm/default.nix")
+      { };
 in
 {
-  imports = [
-    ./hyprland
-    ./xfce
-  ];
+  imports = [ ./hyprland ./xfce ];
 
   services.xserver = {
     enable = true;
-    layout = "us";
-    xkbVariant = "";
     excludePackages = [ pkgs.xterm ];
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
     displayManager.sddm = {
       enable = true;
       theme = "chili";
-      settings = {
-        Theme = {
-          CursorTheme = "macOS-Monterey-White";
-        };
-      };
+      settings = { Theme = { CursorTheme = "macOS-Monterey-White"; }; };
     };
   };
-  
+
   qt = {
     enable = true;
     platformTheme = "qt5ct";
   };
-  
-  environment.systemPackages = with pkgs; [
-    sddm-chili-theme
-  ];
 
+  environment.systemPackages = with pkgs; [ sddm-chili-theme ];
 }
