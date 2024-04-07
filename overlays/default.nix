4c4075cf15603d3e84446eed5916148ba26612c2
RaySlash <stevemathewjoy@gmail.com>
2024-04-07 19:32:52 +1000

    hyprland, misc: add binds for xfce-terminal instead of wezterm

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,4 +7,13 @@
     # ...
     # });
   };
+
+  # When applied, the stable nixpkgs set (declared in the flake inputs) will
+  # be accessible through 'pkgs.stable'
+  stable-packages = final: _prev: {
+    stable = import inputs.nixpkgs-2311 {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
 }
