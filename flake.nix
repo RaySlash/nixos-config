e737fb0c365cd3c99cd8121838eb1d53dd734864
RaySlash <stevemathewjoy@gmail.com>
2024-05-02 18:45:01 +1000

    flake: update yofi
    fix typo

--- flake.nix
+++ flake.nix
@@ -22,52 +22,54 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     nurpkgs.url = "github:nix-community/NUR";
-    yofi.url = "github:rayslash/yofi/update-flake";
+    yofi.url = "github:l4l/yofi";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    systems = import inputs.systems;
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    packages =
-      forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-    formatter =
-      forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-    overlays = import ./overlays {inherit inputs;};
-    nixosModules = import ./modules/nixos;
-    homeManagerModules = import ./modules/home-manager;
+  outputs =
+    { self
+    , nixpkgs
+    , ...
+    } @ inputs:
+    let
+      inherit (self) outputs;
+      systems = import inputs.systems;
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages =
+        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      formatter =
+        forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+      overlays = import ./overlays { inherit inputs; };
+      nixosModules = import ./modules/nixos;
+      homeManagerModules = import ./modules/home-manager;
 
-    nixosConfigurations = {
-      frost = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [./nixos ./nixos/frost/configuration.nix];
-      };
+      nixosConfigurations = {
+        frost = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./nixos ./nixos/frost/configuration.nix ];
+        };
 
-      rpi = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [./nixos ./nixos/rpi/configuration.nix];
-      };
+        rpi = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./nixos ./nixos/rpi/configuration.nix ];
+        };
 
-      dell = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [./nixos ./nixos/dell/configuration.nix];
-      };
+        dell = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./nixos ./nixos/dell/configuration.nix ];
+        };
 
-      live = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        system = "x86_64-linux";
-        modules = [
-          (nixpkgs
-            + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
-          ./nixos
-          ./nixos/iso/configuration.nix
-        ];
+        live = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          system = "x86_64-linux";
+          modules = [
+            (nixpkgs
+              + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+            ./nixos
+            ./nixos/iso/configuration.nix
+          ];
+        };
       };
     };
-  };
 }
--- modules/home-manager/hyprland-addons/default.nix
+++ modules/home-manager/hyprland-addons/default.nix
@@ -11,7 +11,6 @@ in
 
   config = mkIf cfg.enable {
 
-
     services = {
       cliphist.enable = true;
       dunst = {
